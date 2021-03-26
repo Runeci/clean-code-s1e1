d457db73cf29199c45d97bf54521a726e1a83c07
Runeci <kostenokd@mail.com>
2021-03-26 10:40:57 +0300

    refactor: change styles in css-file

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="items__item";
+
     label.innerText=taskString;
     label.className='items__label';
 
